d77dff7083656a05ee0e147c8f68bf325f229f35
igor <igorgarkusha62@gmail.com>
2024-11-03 17:14:09 +0300

    void

--- Task_2/main.cpp
+++ Task_2/main.cpp
@@ -4,8 +4,8 @@
 
 int main() {
     setlocale(LC_ALL, "RU");
-	std::cout << "������� 2. ��� ��������� ����������� ������� ����� ����� ���������, ����������� �� �������.\n";
-	std::cout << "�������� ������� ����� ������������, ������ 453502\n\n";
+	std::cout << "Задание 2. Для заданного одномерного массива найти сумму выражений, вычисляемых по формуле .\n";
+	std::cout << "Выполнил Гаркуша Игорь Вячеславович, группа 453502\n\n\n\n";
     HINSTANCE load;
     load = LoadLibrary(L"DynamicLib.dll");
     typedef int (*readInteger) ();
@@ -18,37 +18,32 @@ int main() {
     recursSumma recursSum;
     recursSum = (recursSumma)GetProcAddress(load, "getRecursiveSum");
     while (true) {
-        std::cout << "������� 1 ��� ������� ���������, 0 ��� �� ����������:\n";
+        std::cout << "Введите 1 для запуска программы, 0 для завершения:\n";
         int type = readInt();
         if (type != 0 && type != 1) {
-            std::cout << "������������ ����\n";
+            std::cout << "Некорректный ввод\n\n";
             continue;
         }
         if (type == 0) {
             break;
         }
         int size;
-        std::cout << "������� ������ �������:\n";
+        std::cout << "Введит размер массива:\n";
         while (true) {
             size = readInt();
             if (size <= 0) {
-                std::cout << "������������ ����\n";
+                std::cout << "Некорректный ввод\n";
                 continue;
             }
             break;
         }
-        //������������ ��� ��� ���������� �������� ������� �� ���������� ������� ������� �� 1
-        //�����������, ������ ��� ����������� ����� ������� ���������� � ������� 1
         int step = size / 2;
         size += 1;
-        //�������� �����
         double* arr = new double[size];
-        //��������� ������
         arrayFiller(arr, size);
-        //������������ ���������
         double res = recursSum(arr, size, step);
         delete[] arr;
-        std::cout << "��������� ������ ���������: " << res << '\n';
+        std::cout << "Результат работы программы: " << res << '\n';
     }
 	return 0; 
 }
